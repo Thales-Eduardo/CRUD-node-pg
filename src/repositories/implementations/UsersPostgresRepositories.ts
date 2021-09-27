fb46c5d174786066df40ce76bb0cb49b6d968a46
Thales Eduardo <thalesdev22@gmail.com>
2021-09-27 15:04:24 -0300

    Repositório concluído.

--- src/repositories/implementations/UsersPostgresRepositories.ts
+++ src/repositories/implementations/UsersPostgresRepositories.ts
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 import { createConection } from '../../database/conection';
+import { User } from '../../entities/User';
 
 import { IPostgresRepository } from '../methods/IPostgresRepository';
 import { IPostgresRepositoriesDTO } from '../dtos/IPostgresRepositoriesDTO';
@@ -21,5 +22,27 @@ export class UsersPostgresRepositories implements IPostgresRepository {
       'INSERT INTO USERS (ID, NAME, EMAIL) VALUES ($1, $2, $3)',
       [id, name, email],
     );
+  }
+
+  public async findById(idUser: string): Promise<User> {
+    const { rows } = await this.client.query(
+      'SELECT * FROM USERS WHERE ID = $1 LIMIT 1',
+      [idUser],
+    );
+
+    if (!rows) {
+      throw new AppErrors('Id inválido!');
+    }
+
+    const { id, name, email, date } = rows[0];
+
+    const user = {
+      id,
+      name,
+      email,
+      date,
+    };
+
+    return user;
   }
 }
--- src/repositories/methods/IPostgresRepository.ts
+++ src/repositories/methods/IPostgresRepository.ts
@@ -1,5 +1,7 @@
+import { User } from '../../entities/User';
 import { IPostgresRepositoriesDTO } from '../dtos/IPostgresRepositoriesDTO';
 
 export interface IPostgresRepository {
   createUser(data: IPostgresRepositoriesDTO): Promise<void>;
+  findById(idUser: string): Promise<User>;
 }
