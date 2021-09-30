b2626347af9a4d8d07214e4fd759e867a7f7f1c0
Thales Eduardo <thalesdev22@gmail.com>
2021-09-30 13:03:19 -0300

    add teste unitário para updateUser.

---
+++ src/useCases/updateUser/UpdateUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { FakeUserRepositories } from '../../repositories/fakes/FakeUserRepositories';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { UpdateUserUseCase } from './UpdateUserUseCase';
+
+import { AppErrors } from '../../errors/AppErrors';
+
+let createUserUseCase: CreateUserUseCase;
+let fakeUserRepositories: FakeUserRepositories;
+let updateUserUseCase: UpdateUserUseCase;
+
+describe('updateUserUseCase', () => {
+  beforeEach(() => {
+    fakeUserRepositories = new FakeUserRepositories();
+
+    createUserUseCase = new CreateUserUseCase(fakeUserRepositories);
+    updateUserUseCase = new UpdateUserUseCase(fakeUserRepositories);
+  });
+
+  it('Must be able to list user by id', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'thales',
+      email: 'thalesdev22@gmail.com',
+    });
+
+    const response = await updateUserUseCase.execute({
+      id: user.id,
+      name: 'thales22',
+      email: 'thalesdev@gmail.com',
+    });
+
+    expect(response).toBe(undefined);
+  });
+
+  it('Not must be able to list user by id', async () => {
+    await expect(
+      updateUserUseCase.execute({ id: '', name: '', email: '' }),
+    ).rejects.toBeInstanceOf(AppErrors);
+    await expect(
+      updateUserUseCase.execute({ id: 'fake', name: '', email: '' }),
+    ).rejects.toBeInstanceOf(AppErrors);
+  });
+});
